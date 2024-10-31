205601325aa13b317ca89dd04bc125347849321a
FerreiraMCastro <marciasilvacf1989@gmail.com>
2024-10-31 19:22:27 -0300

    exercícios atividade proz 31/10/24

---
+++ mostra-andar.py
@@ -0,0 +1,15 @@
+
+for i in range(1,21):
+     if(i != 13):
+      print(i)
+
+for i in range(20,1,-1):
+    if(i != 13):
+     print(i)
+    
+
+andar = 1
+while(andar <= 20):
+    if(andar != 13):
+     print(andar)
+    andar+=1 
